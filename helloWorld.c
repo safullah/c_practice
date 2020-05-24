0ab37b382e22c60e72c6f32d56858de3ef91fb32
Saifur Rahman <saifur676@gmail.com>
2020-05-24 08:18:47 +0200

    fist commit

---
+++ helloWorld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    char *name = "Saifur Rahman";
+    printf("Hello world. My name is %s\n", name);
+    git@github.com:safullah/c_practice.git
+    echo "# c_practice" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:safullah/c_practice.git
+git push -u origin master
+  return EXIT_SUCCESS;
+}
